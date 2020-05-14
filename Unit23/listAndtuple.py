f037d46b5fcb2c363aab8015f6a4ea7f5927a82b
YEWOONY <dge04206@gmail.com>
2020-05-14 16:22:12 +0900

    modified code

--- Unit23/listAndtuple.py
+++ Unit23/listAndtuple.py
@@ -1,17 +1,17 @@
-#tuple in tuple
+# tuple in tuple
 a = ((1,2), (3,4))
 print('tuple in tuple:', a)
 
-#list in tupleㅎ
+# list in tuple
 b = ([1,2], [3,4])
 print('\nlist in tuple:', b)
-#안쪽에 있는 리스트의 원소만 변경 가능
+# 안쪽에 있는 리스트의 원소만 변경 가능
 b[0][0] = 5
 print('After:', b)
 
-#tuple in list
+# tuple in list
 c = [(1,2), (3,4)]
 print('\ntuple in list:', c)
-#바깥쪽에 있는 리스트의 원소만 변경 가능
+# 바깥쪽에 있는 리스트의 원소만 변경 가능
 c[0] = (5,6)
 print('After:', c)
